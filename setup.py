1f5048efcb89b97d1f522317c08c4a8614b0f042
Rahil-mahini <103961498+Rahil-mahini@users.noreply.github.com>
2024-08-16 17:31:20 -0500

    Add package_dir in setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ setup(
     name='combinatorixPy',
     version='1.0.0',
     packages=find_packages( where = "src"),
+    package_dir={'': 'src'},
     description='Development of Numerical Features/Descriptors to Describe Complex Materials for Machine Learning Modeling',
     long_description= desscription,
     long_description_content_type='text/markdown',
     author='Rahel Mahini',
     author_email='Rahil.Ashtarimahini@ndsu.edu',
-    url='https://github.com/Rahil-mahini/combinatorial',
+    url='https://github.com/bakhras/MixtureDescriptors-II',
     license = " GPL-3.0",
 
     classifiers=[
@@ -22,11 +23,10 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',     
         'Topic :: Scientific/Engineering :: Chemistry',
-        'License :: OSI Approved :: GNU General Public License, version 3 ',
+        'License :: OSI Approved :: GNU General Public License, version 3',
         'Programming Language :: Python :: 3.10',
- 
-      
     ],
+
     python_requires='>=3.6',
     install_requires = [
         # List your project's dependencies here.
