9bf8830efa78ad631c0be113a76766ae41fdaa46
Rahil-mahini <103961498+Rahil-mahini@users.noreply.github.com>
2024-08-16 15:24:55 -0500

    Update the setup.py add the twine and change the classifiers

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages( where = "src"),
     description='Development of Numerical Features/Descriptors to Describe Complex Materials for Machine Learning Modeling',
     long_description_content_type='text/markdown',
-    author='Rahil Ashtari',
+    author='Rahl Mahini',
     author_email='Rahil.Ashtarimahini@ndsu.edu',
     url='https://github.com/Rahil-mahini/combinatorial',
     license = " GPL-3.0",
@@ -17,8 +17,8 @@ setup(
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: SCIENCE/RESEARCH',
-        'Topic :: CIENTIFIC/ENGINEERING :: CHEMISTRY',
+        'Intended Audience :: Science/Research',     
+        'Topic :: Scientific/Engineering :: Chemistry',
         'License :: OSI Approved :: GNU General Public License, version 3 ',
         'Programming Language :: Python :: 3.10',
  
@@ -33,10 +33,12 @@ setup(
         'dask.array',
         'dask.dataframe',
         'dask.distributed',
+    ],
 
+    extras_requires={
+        "dev":["twine>=4.0.2"]
+        }
 
-
-    ]
     
 )
 
